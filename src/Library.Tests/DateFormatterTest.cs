68cbc56a551197098beb352d28735e8b8f7df9b9
Pau <pauxxann@gmail.com>
2025-09-10 11:54:55 -0300

    tests

---
+++ src/Library.Tests/DateFormatterTest.cs
@@ -0,0 +1,14 @@
+﻿using Ucu.Poo.TestDateFormat;
+
+namespace Library.Tests;
+
+[NUnit.Framework.TestFixture]
+public class DateFormatterTest
+{
+
+    [NUnit.Framework.Test]
+    public void METHOD()
+    {
+        
+    }
+}
